0aab73c4c007cb8c59429dd8b4141c9005fd29b0
Victor Landa <victorlanda.dev@gmail.com>
2020-02-04 10:54:32 -0600

    Arrow Functions, Promesas y Parámetros en objetos.

--- src/es6/index.js
+++ src/es6/index.js
@@ -92,3 +92,77 @@ console.log(name, age, country);
   // const nunca va a cambiar su valor
   const a = 'b';
   a = 'a'; // Error
+
+  // -- -- -- Arrow Functions, Promesas y Parámetros en objetos -- -- --
+  // Creaación de objetos ES5
+  let name = 'Franklin';
+  let age = 29;
+
+  obj = { name: name, age: age };
+
+  // Creaación de objetos ES6
+  // Aquí toma por defecto la key y value de los elementos.
+  obj2 = { name, age }
+  console.log(obj2);
+
+  // Arrow Function
+  /**
+   * Estas funciones vienen a solventar un 
+   * problema en particular, a trabajar con dos elementos, manteniendo 
+   * una sintaxis reducida y un this no vinculable.
+   */
+
+   // Creamos un arreglo de objetos
+  const names = [
+    { name: 'Franklin', age: 29 },
+    { name: 'Trevor', age: 42 },
+    { name: 'Michael', age: 40 },
+  ];
+
+  // Vamos a iterear sobre nuestro objeto utilizando el método map()
+  // A map le pasamos una función anónima y luego recibiamos el elementos dentro de la función.
+  // Item será el elemento que queremos traer.
+  let listOfNames = names.map(function(item) {
+    // Por cada uno de los elementos de este arreglo obtenemos el nombre.
+    console.log(item.name);
+  });
+
+  // Las Arrow Function son funciones anónimas.
+  let listOfNames2 = names.map(item => console.log(item.name));
+  let listOfNames3 = names.map(item => {
+    console.log(item.name);
+  })
+
+  const listOfNames4 = (name, age, country) => {
+    // Código que vamos a ocupar para listOfNames3
+  }
+
+  // Cuando sólo estamos pasando un parámetro
+  const listOfNames5 = name => {
+    // Block code
+  }
+
+  // Estamos generando una función que recibe como valor `num` y lo que hacemos es 
+  // multiplicar num por si mismo, así ya no tenemos que generar un return.
+  const square = num => num * num;
+
+  // -- -- -- Promises -- -- --
+  /**
+   * Con las promesas nosotros vamos a trabajar el asincronismo.
+   * Una promesa significa (como lo dice su nombre) que algo va a 
+   * pasar.
+   */
+  const firstPromise = () => {
+    // Vamos a retornar una promesa con dos elementos que por lo regular necesitan (resolve, reject);
+    return new Promise((resolve, reject) => {
+      if(true) {
+        resolve('Resolve!');
+      } else {
+        reject('Algo salió mal');
+      }
+     });
+  }
+
+  firstPromise()
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
